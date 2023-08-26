0a6507cd5fccb8d4fb73cd3d55845a1e9ecff3c6
Sasa Savic Perusina <sasa@bubulazy.com>
2023-08-26 14:16:27 +0100

    Working on Toggle Btn component, still

--- src/components/Thermostato/Thermostato.jsx
+++ src/components/Thermostato/Thermostato.jsx
@@ -9,7 +9,13 @@ export default function Thermostato() {
         <h1 className={styles.h1}>ThermoKing</h1>
         <div className={styles.display}>25</div>
         <div className={styles.buttons_group}>
-          <ToggleButton />
+          <ToggleButton
+            label="Mode"
+            options={[
+              { label: "C", value: "celsius" },
+              { label: "F", value: "fahrenheit" },
+            ]}
+          />
           <div className={styles.chevrons_group}>
             <button className={styles.chevron_btn}>
               <ChevronDown size={35} />
--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -1,10 +1,15 @@
+import { useId } from "react";
 import styles from "./ToggleButton.module.css";
 
-export default function ToggleButton() {
+export default function ToggleButton({ label, options }) {
+  const id = useId();
+
   return (
-    <div>
-      <label htmlFor="">MODE</label>
-      <input type="checkmark" />
+    <div className={styles.container}>
+      <div className={styles.toggle_label}>{label}</div>
+      <div className={styles.toggle_button}>
+        {options.map(({ label, value }) => {})}
+      </div>
     </div>
   );
 }
